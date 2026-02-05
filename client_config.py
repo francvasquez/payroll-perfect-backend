155ac6245f420bbf72868bbfa26dc09596450376
Frank V <francvasquez@gmail.com>
2026-02-05 11:14:19 -0800

    ingo logs

--- client_config.py
+++ client_config.py
@@ -1,18 +1,18 @@
+# This is a check at the very top of ta_process
 PP_REQUIRED_COLUMNS = [
     "ID",  # unique per employee. Important: First three chars are Location Code
     "Employee",  # employee name (full name)
     "In Punch",  # containing both date and time info of the punch
     "Out Punch",  # containing both date and time info of the punch
+    "Totaled Amount",  # total hours worked in the punch (decimal hours)
 ]
 
-
-# "mappings" are column remapping for specific client data.
-# "drop_columns" are junk columns to be dropped at the top (won't be processed at all).
-# note that system created helper columns are dropped before database write in save_to_database_fast function
+# Also at the very top, drop junk columns and map col names interface
+# Note: Python created helper columns are dropped before database write in save_to_database_fast function
 CLIENT_CONFIGS = {
     "demo_client": {  # BH
         "mappings": {"Employee": "Employee", "ID": "ID"},
-        "drop_columns": ["Org Path", "Date/Time", "Totaled Amount"],
+        "drop_columns": ["Org Path", "Date/Time"],
     },
     "client_b": {  # sample client
         "mappings": {"Staff_No": "ID", "Clock_In": "In Punch"},
@@ -20,8 +20,8 @@ CLIENT_CONFIGS = {
     },
 }
 
-# columns that the user should not be able to see on the pulldown,
-# either because they are required or junk
+# For DB UI: Columns that the user should not be able to see on the pulldown,
+# either because they are required (already there) or just fluff
 EXCLUDE_FROM_PULLDOWN = {
     "ID",  # req
     "Employee",  # req
