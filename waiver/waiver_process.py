4b6f7bb6b3409b834c539999e6ef602760cf5d04
Frank V <francvasquez@gmail.com>
2026-03-12 16:27:40 -0700

    Waiver will remove dup IDs instead of Names

--- waiver/waiver_process.py
+++ waiver/waiver_process.py
@@ -4,7 +4,7 @@ import numpy as np
 def process_waiver(df):
 
     # Remove dups otherwise won't work as a lookup reference
-    processed_waiver_df = df.drop_duplicates(subset="Name", keep="first").copy()
+    processed_waiver_df = df.drop_duplicates(subset="ID", keep="first").copy()
 
     # Check_Pure becomes Boolean
     processed_waiver_df["Has_Waiver_Bool"] = np.where(
